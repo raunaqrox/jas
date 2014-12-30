54d971b63a4bf56f2a63fe1e612efed7d3540505
Sahebjot Singh <sahebjot94@gmail.com>
2014-12-30 14:18:21 +0530

    basic extra selector added, remove selector added, few bugs

--- public/js/selector.js
+++ public/js/selector.js
@@ -1,26 +1,47 @@
 $(document).ready(function(){
     var myHtml = $('#htmlContainer').text();
     html = $.parseHTML(myHtml);
-    var select = $('#select_1');
-    var selector = $('#selector_1');
-    var container = $('#htmlContainer h5');
-    var originalHtml = container.text();
-    var nOfInp = 1;
-    var moreSelectors = $('#moreSelec');
-
-    select.on('click', function(){
-        var mySelector = selector.val();
+    var selector; 
+    var container; 
+    var originalHtml; 
+    var inputs; 
+    var nOfInp;
+    var moreSelectors;
+    var options;
+    function selectorResult(){
+        var mySelector = $(this).val();
         if(mySelector.length!==0){
             var tex = $(html).find(mySelector).html();
             container.text(tex); 
         }else{
             container.text(originalHtml);
         }
-    });
-    moreSelectors.on('click', function(){
+    }
+
+    function removeThis(){
+        var temp = $(this).attr('id').split('_')[1];
+        $('#'+temp).remove();
+    }
+
+    function addInput(){
         nOfInp+=1;
-        var toAppend = "<br><input id='selector_'"+nOfInp+" placeholder='   selector here'><button id='select_'"+nOfInp+">selector</button>'";
-        var options = $('#options');
+        var toAppend = "<br><div id='"+nOfInp+"'><input class='fOpInp' id='selector_'"+nOfInp+" placeholder='   selector "+nOfInp+" here'><button id='X_"+nOfInp+"'>X</button></div>";
         options.append(toAppend);
-    });
+        inputs = $('input');
+        var x = $('#X_'+nOfInp);
+        $(x).on('click', removeThis);
+        $(inputs[nOfInp-1]).on('change input', selectorResult);
+    }
+    function init(){
+        options = $('#options');
+        moreSelectors = $('#moreSelec');
+        nOfInp = 1;
+        inputs = $('input');
+        selector = $('#selector_1');
+        container = $('#htmlContainer h5');
+        originalHtml = container.text();
+        inputs.on('input change', selectorResult);
+        moreSelectors.on('click', addInput);
+    }
+    init();
 });
